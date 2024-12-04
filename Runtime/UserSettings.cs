45ef6c123252a02e414817a7ff4bc3a48faa68e7
Niko Skakun <skakun.dev@gmail.com>
2024-12-04 22:59:48 +0200

    Initial commit

---
+++ Runtime/UserSettings.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+namespace Sloyd.WebAPI
+{
+    [CreateAssetMenu(fileName = AssetName, menuName = "Sloyd/UserSettings", order = 0)]
+    public class UserSettings : ScriptableObject
+    {
+        public const string AssetName = "SloydUserSettings";
+        
+        public string ClientId = string.Empty;
+        public string ClientSecret = string.Empty;
+
+        public SloydClientAPI.PromptModifier DefaultPromptModifier = SloydClientAPI.PromptModifier.None;
+        public bool AutoPlacement = true;
+        public bool EnableLogging = true;
+        public string CachePath = string.Empty;
+        
+        [NonSerialized]
+        public string Token = string.Empty;
+    }
+}
